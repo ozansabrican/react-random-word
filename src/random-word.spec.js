9c199c2ac5f4a6d745af088da3375d21456e8ff7
Sabrican Ozan <sabrican.ozan@gmail.com>
2016-08-30 23:40:38 +0200

    test: add simple test

---
+++ src/random-word.spec.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import RandomWord from './random-word.js';
+
+test('Random word renders at least one span', (t) => {
+  const wrapper = shallow(<RandomWord word="test"/>);
+  const actual = wrapper.find('span').length;
+  const expected = 1;
+
+  t.is(actual, expected);
+});
